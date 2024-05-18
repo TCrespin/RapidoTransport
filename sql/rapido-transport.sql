13f75e307134253e05e2ef12698c91e43f19b48d
TCrespin <crespinstart@gmail.com>
2024-05-18 21:26:45 +0100

    Add files via upload

---
+++ sql/rapido-transport.sql
@@ -0,0 +1,83 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : sam. 18 mai 2024 à 20:23
+-- Version du serveur : 8.3.0
+-- Version de PHP : 8.3.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `rapido-transport`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `chauffeurs`
+--
+
+DROP TABLE IF EXISTS `chauffeurs`;
+CREATE TABLE IF NOT EXISTS `chauffeurs` (
+  `chauffeur_id` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(20) NOT NULL,
+  `prenoms` varchar(50) NOT NULL,
+  `telephone` int NOT NULL,
+  `sexe` char(1) NOT NULL,
+  `disponible` tinyint(1) NOT NULL,
+  PRIMARY KEY (`chauffeur_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `courses`
+--
+
+DROP TABLE IF EXISTS `courses`;
+CREATE TABLE IF NOT EXISTS `courses` (
+  `course_id` int NOT NULL AUTO_INCREMENT,
+  `point_depart` varchar(255) NOT NULL,
+  `point_arrivee` varchar(255) NOT NULL,
+  `date_heure` varchar(255) NOT NULL,
+  `chauffeur_id` int NOT NULL,
+  `statut` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`course_id`),
+  KEY `chauffeur_id` (`chauffeur_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `mail` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`user_id`, `mail`, `password`) VALUES
+(3, 'rapido@rapido.com', 'rapido');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
